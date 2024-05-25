a1b32993ad152bf408d616c643be1a2d3fb77c98
Diego Jerez Lira <dljerez@uc.cl>
2024-05-25 15:13:12 +0200

    Madame la Purr 2.0 Bug fixed

--- js/pages/magic.js
+++ js/pages/magic.js
@@ -1,41 +1,45 @@
-//Primary Function //
-function MagicBall(question){
-    let answer = "";
-    let number = Math.floor(Math.random()*3)+1;
-    if (number == 1){
-        answer = "The answer for your question " + question + " is No";
-    }
-    else if(number == 2){
-        answer ="The answer for your question " +  question + " is Yes";
-    }
-    else{
-        answer ="The answer for your question " +  question + " is maybe";
-    }
+
+
+//Global variables
+let magicDialog = document.getElementById("magicDialog");
+
+// Asking for name
+let nameAsking = document.getElementById("nameAsking");
+nameAsking.addEventListener("submit", welcoming);
+
+function welcoming(e){
+    e.preventDefault();
+    let userName = document.getElementById("name").value;
+    let userWelcome= "Hello " + userName + ", please, ask me your question. Remember, just Yes/No questions";
+    talk("magicDialog", userWelcome);
+    nameAsking.style.display = "none";
+    questions.style.display = "flex";
 };
 
-//Asking for name
-document.getElementById("questions").addEventListener('submit', function(event) {
-    event.preventDefault();
-    let user = document.getElementById("questions")
-    const userName = document.getElementById('name').value;
-    talk("magic_dialog","Hello " + userName)
-    talk("first_question", "Please, write your question. Remember I cant only predict Yes/No questions")
-    let name = document.getElementById("name");
-    name.id = "question"
-    name.placeholder = "Write your question"
-    name.value=""
-    user.id = "userQuestions"
-});
+//asking questions
+let questionAsking = document.getElementById("questionAsking")
+questions.addEventListener("submit", magic);
 
-// Questions
-document.addEventListener('submit', function(event) {
-    event.preventDefault();
-    if (event.target.id === "userQuestions") { 
-        const UserQuestion = document.getElementById('question').value;
-        const answer = MagicBall(UserQuestion); 
-        talk("first_question", answer);
-        let question1 = document.getElementById("question");
-        question1.placeholder = "Write another question";
-        question1.value = "";
+// Primary Function
+function MagicBall(question) {
+    let answer = "";
+    let number = Math.floor(Math.random() * 3) + 1;
+    if (number === 1) {
+        answer = "The answer for your question " + question + " is No";
+    } else if (number === 2) {
+        answer = "The answer for your question " + question + " is Yes";
+    } else {
+        answer = "The answer for your question " + question + " is maybe";
     }
-});
+    return answer;
+}
+
+// Secundary function
+ function magic(e){
+    e.preventDefault();
+    let questionArea = document.getElementById("question");
+    let question = document.getElementById("question").value;
+    let answer= MagicBall(question);
+    talk("magicDialog", answer);
+    questionArea.value = "";
+ }
