c844b671f5ba60e259f768c6e23f98cb17e8eeac
Diego Jerez Lira <dljerez@uc.cl>
2024-05-25 19:19:17 +0200

    cathespiare v. 1.0

--- js/main.js
+++ js/main.js
@@ -19,69 +19,8 @@ const cathespiare =new gato("Cathespiare", "Naranja", "Escribir una historia", "
 const miauLaCasan =new gato("Miau La Casan", "Rubio", "Dar consejos", "Recordá que sos unic@ y grande");
 const ariel =new gato("Ariel", "Gris", "Hablar sobre los otros gatos", "Soy el dueño de este sitio, lo cree para que puedas divertirte");
 
-/* Primer gato */
-let pregunta= "";
-let repeticion= 0;
-
-function bolaMagica(){
-    let numero = Math.floor(Math.random()*3)+1;
-    if (numero == 1){
-        hablar("la respuesta para " + pregunta + " es No" );
-    }
-    else if(numero == 2){
-        hablar("la respuesta para " + pregunta + " es Sí" );
-    }
-    else{
-        hablar("La respuesta para " + pregunta + " es Tal vez");
-    }
-};
-function gatoAdivino(){
-    hablar("Tengo una respuesta para todas tus preguntas, pero solo de sí o no");
-    pregunta= prompt("Escribe tu pregunta, recuerda solo de si o no");
-    bolaMagica();
-        repeticion= prompt("Tienes otra pregunta? 1. Si 2. No")
-        if (repeticion == 1){
-           control = 1;
-        }
-        else if (repeticion == 2){
-            hablar("Me alegro " + nombre + " Ten un excelente día");
-            control= control+1;
-            }
-        else{
-            hablar("Opción invalida");
-            control= 1;
-            }
-}
 
 /* Segundo Gato */
-const palabras=[];
-let repeticionCuento = 0;
-function lapiz(){
-    palabras.splice(0,6);
-    palabras.push(prompt("Dame una estación de año"));
-    palabras.push(prompt("Dame un lugar"));
-    palabras.push(prompt("Dame un verbo"));
-    palabras.push(prompt("Dame un adjetivo en femenino"));
-    palabras.push(prompt("Dame una parte del cuerpo con su artículo"));
-    palabras.push(prompt("Dame un verbo"));
-    hablar("Era una oscura noche de " + palabras[0] + " Cuando " + nombre + " entró a " + palabras[1]+ ", a lo lejos vio una persona " + palabras[2]+ ", vio una estatua que era " + palabras[3] + ", La estatua se levanto corrió donde la persona y le cortó " + palabras[4]+
-    ", lo único que " + nombre + " puedo hacer fue " + palabras[5]);
-}
-function gatoEscritor(){
-    hablar("Ayúdame a construir mi historia, dame palabras para completar mi relato");
-    lapiz();
-    repeticionCuento= prompt("¿Quiéres intentar denuevo? 1. Si 2. No");
-    if (repeticionCuento == 1){
-        control= 2;}
-    else if (repeticionCuento == 2){
-        hablar("Asi que ya quedaste asustad@ " + nombre + " Ten un excelente día");
-        control= 10;
-    }
-    else{
-        hablar("Opción invalida");
-        control= 2;
-    }
-}
 
 /* Tercer gato */
     function gatoConsejero(){
---
+++ js/pages/writting.js
@@ -0,0 +1,75 @@
+// Global variables
+const words=[];
+let control = 0
+let textArea = document.getElementById("words");
+let userName= ""
+
+//NameAsking
+
+let nameAsking = document.getElementById("nameAsking");
+nameAsking.addEventListener("submit", welcoming);
+
+function welcoming(e){
+    e.preventDefault();
+    let userName = document.getElementById("name").value;
+    nameAsking.style.display = "none";
+    wordsAsking.style.display = "flex";
+};
+
+//AddWords
+
+wordsAsking.addEventListener("submit", addWord);
+
+function addWord(e){
+    e.preventDefault();
+    let word = textArea.value;
+    words.push(word);
+    textArea.value = ""
+    control = control + 1;
+    //Steps controller
+if (control == 0){
+    talk("writtingDialog", "Give me a season");
+    textArea.placeholder = "Write a season"
+}
+else if (control == 1){
+    talk("writtingDialog", "Give me a place");
+    textArea.placeholder = "Write a place";
+}
+else if (control == 2){
+    talk("writtingDialog", "Give me a verb with ing");
+    textArea.placeholder = "Write a verb + ing";
+}
+else if (control == 3){
+    talk("writtingDialog", "Give me a adjetive");
+    textArea.placeholder = "Write a adjetive";
+}
+else if (control == 4){
+    talk("writtingDialog", "Give me a part of the body");
+    textArea.placeholder = "Write a part of the body";
+}
+else if (control == 5){
+    talk("writtingDialog", "Give me another verb");
+    textArea.placeholder = "Write another verb";
+}
+else{
+    let final= pencil();
+    talk("answers", final);
+    wordsAsking.style.display = "none";
+
+
+};
+};
+function pencil(){
+    let story = "There was a dark night of " + words[0] + " when " + userName + " enters to " + words[1] + ", suddenly, " + userName + " saw at the end of the " + words[1] + " a person, who was " + words[2] + ". In the right corner, a big statue " + userName + " said, it´s a " + words[3] + " statue. Suddenly, the statue stand up, ran and cut the person´s " + words[4] + ". The only thing that "+ userName + " can do, was " + words[5];
+    return story;
+};
+
+// Reset
+
+wordsAsking.addEventListener("reset", eraseWords);
+
+function eraseWords(){
+    e.preventDefault();
+    words.splice(0,6);
+    control = 0;
+}
